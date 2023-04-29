d6c38cb43b5e6e87177194961f08e94817636698
Shuvajid Das <77564076+Shuvajid@users.noreply.github.com>
2023-04-29 17:39:50 +0600

    Update LSTM.py

--- pages/LSTM.py
+++ pages/LSTM.py
@@ -196,7 +196,7 @@ def partition_dataset(sequence_length, data):
 # Generate training data and test data
 x_train, y_train = partition_dataset(sequence_length, train_data)
 x_test, y_test = partition_dataset(sequence_length, test_data)
-
+from keras.models import Sequential
 model = Sequential()
 
 # Model with n_neurons = inputshape Timestamps, each with x_train.shape[2] variables
